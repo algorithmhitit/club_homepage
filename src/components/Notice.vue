5be1280e84a3882b2bd807a50396daccbe3dda3e
insu Jeong <wjddlstn0614@naver.com>
2019-05-20 13:20:35 +0900

    공지사항 table 만드는 중

--- src/components/Notice.vue
+++ src/components/Notice.vue
@@ -4,6 +4,28 @@
 <template>
     <div>
         <h1>공지사항</h1>
+        <h1>Contact Management</h1>
+        <div id='vueapp'>
+
+            <table border='1' width='100%' style='border-collapse: collapse;'>
+            <tr>
+                <th>Name</th>
+                <th>Email</th>
+                <th>Country</th>
+                <th>City</th>
+                <th>Job</th>
+
+            </tr>
+
+            <!--<tr v-for='contact in contacts'>
+                <td>{{ contact.name }}</td>
+                <td>{{ contact.email }}</td>
+                <td>{{ contact.country }}</td>
+                <td>{{ contact.city }}</td>
+                <td>{{ contact.job }}</td>
+            </tr>-->
+            </table>
+        </div>
     </div>
 </template>
 
@@ -12,6 +34,31 @@
 export default {
     name : "Notice"
 }
+/*
+var app = new Vue({
+  el: '#vueapp',
+  data: {
+      name: '',
+      email: '',
+      country: '',
+      city: '',
+      job: '',
+      contacts: []
+  },
+  mounted: function () {
+    console.log('Hello from Vue!')
+    this.getContacts()
+  },
+
+  methods: {
+    getContacts: function(){
+    },
+    createContact: function(){
+    },
+    resetForm: function(){
+    }
+  }
+})*/ 
 </script>
 
 <!--공지사항 style code-->
