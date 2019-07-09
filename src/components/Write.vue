6a45d982d4bd6b1e2649d0530cee159fb14a31d7
insu Jeong <wjddlstn0614@naver.com>
2019-07-09 17:40:12 +0900

    게시판 editor vue2-editor api 를 사용하였다.

--- src/components/Write.vue
+++ src/components/Write.vue
@@ -1,13 +1,19 @@
 <template>
     <div>
         <p>wirte page router test 중입니다.</p>
+        <vue-editor v-model="content"></vue-editor>
     </div>
 </template>
 
 <script>
+import { VueEditor } from "vue2-editor"; //vue 게시글 작성 editor api
+
 //console.log("Write page, 게시글 작성 페이지")
 export default {
     name: 'Write',
+    components: {
+        VueEditor
+    },
     data() {
         return {
 
